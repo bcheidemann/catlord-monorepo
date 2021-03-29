4fe2f6d29141f9551a397a07f20bad8823b5e4d3
Ben Heidemann <ben@heidemann.co.uk>
2021-03-29 13:55:22 +0000

    chore: add storybook configuration for components

---
+++ libs/components/src/lib/screen/screen.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Screen, ScreenProps } from './screen';
+
+export default {
+  component: Screen,
+  title: 'Screen',
+};
+
+export const primary = () => {
+  /* eslint-disable-next-line */
+  const props: ScreenProps = {};
+
+  return <Screen />;
+};
